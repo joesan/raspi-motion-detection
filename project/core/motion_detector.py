f62584a5284eac7bf70eaa028749d3c99a53ceee
codeintheopen@gmail.com <codeintheopen@gmail.com>
2018-11-11 18:10:01 +0100

    copy commit

--- project/core/motion_detector.py
+++ project/core/motion_detector.py
@@ -1,154 +1,154 @@
-'''
-# import the necessary packages
-# from core.tempimage import TempImage
-# from picamera.array import PiRGBArray
-# from picamera import PiCamera
-# import argparse
-# import warnings
-# import datetime
+# CCTV surveillance with Motion Detection for Raspberry Pi 3 B+ using a Pi Camera
+#
+# Code adapted from
+# https://www.pyimagesearch.com/2015/06/01/home-surveillance-and-motion-detection-with-the-raspberry-pi-python-and-opencv/
+#
+# TODO: Modification & Documentation
+#
+from tempimage import TempImage
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import argparse
+import warnings
+import datetime
 import dropbox
 import imutils
 import json
 import time
 import cv2
 
-
 # construct the argument parser and parse the arguments
-def parse_arguments():
-    ap = argparse.ArgumentParser()
-    ap.add_argument("-c", "--conf", required=True,
-                    help="path to the JSON configuration file")
-    args = vars(ap.parse_args())
-    return args
+ap = argparse.ArgumentParser()
+ap.add_argument("-c", "--conf", required=True,
+                help="path to the JSON configuration file")
+args = vars(ap.parse_args())
 
-
-# filter warnings, load configuration and initialize Dropbox client
+# filter warnings, load the configuration and initialize the Dropbox
+# client
 warnings.filterwarnings("ignore")
-conf = json.load(open(parse_arguments["conf"]))
+conf = json.load(open(args["conf"]))
+client = None
 
+# check to see if the Dropbox should be used
+if conf["use_dropbox"]:
+    # connect to dropbox and start the session authorization process
+    client = dropbox.Dropbox(conf["dropbox_access_token"])
+    print("[SUCCESS] dropbox account linked")
 
-def init_dropbox_client():
-    client = None
-    # check to see if the Dropbox should be used
-    if conf["use_dropbox"]:
-        # connect to Dropbox and start the session authorization process
-        client = dropbox.Dropbox(conf["dropbox_access_token"])
-        print("[SUCCESS] dropbox account linked")
-    return client
+# initialize the camera and grab a reference to the raw camera capture
+camera = PiCamera()
+camera.resolution = tuple(conf["resolution"])
+camera.framerate = conf["fps"]
+rawCapture = PiRGBArray(camera, size=tuple(conf["resolution"]))
 
+# allow the camera to warmup, then initialize the average frame, last
+# uploaded timestamp, and frame motion counter
+print("[INFO] warming up...")
+time.sleep(conf["camera_warmup_time"])
+avg = None
+lastUploaded = datetime.datetime.now()
+motionCounter = 0
 
-def init_picamera_and_detect_motion():
-    camera = PiCamera()
-    camera.resolution = tuple(conf["resolution"])
-    camera.framerate = conf["fps"]
-    raw_capture = PiRGBArray(camera, size=tuple(conf["resolution"]))
+# capture frames from the camera
+for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+    # grab the raw NumPy array representing the image and initialize
+    # the timestamp and occupied/unoccupied text
+    frame = f.array
+    timestamp = datetime.datetime.now()
+    text = "Unoccupied"
 
-    # allow the camera to warmup, then initialize the average frame, last
-    # uploaded timestamp, and frame motion counter
-    print("[INFO] warming up...")
-    time.sleep(conf["camera_warmup_time"])
-    avg = None
-    last_uploaded = datetime.datetime.now()
-    motion_counter = 0
+    # resize the frame, convert it to grayscale, and blur it
+    frame = imutils.resize(frame, width=500)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
-    # capture frames from the camera
-    for f in camera.capture_continuous(raw_capture, format="bgr", use_video_port=True):
-        # grab the raw NumPy array representing the image and initialize
-        # the timestamp and occupied/unoccupied text
-        frame = f.array
-        timestamp = datetime.datetime.now()
-        text = "Unoccupied"
+    # if the average frame is None, initialize it
+    if avg is None:
+        print("[INFO] starting background model...")
+        avg = gray.copy().astype("float")
+        rawCapture.truncate(0)
+        continue
 
-        # resize the frame, convert it to grayscale, and blur it
-        frame = imutils.resize(frame, width=500)
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        gray = cv2.GaussianBlur(gray, (21, 21), 0)
+    # accumulate the weighted average between the current frame and
+    # previous frames, then compute the difference between the current
+    # frame and running average
+    cv2.accumulateWeighted(gray, avg, 0.5)
+    frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(avg))
 
-        # if the average frame is None, initialize it
-        if avg is None:
-            print("[INFO] starting background model...")
-            avg = gray.copy().astype("float")
-            raw_capture.truncate(0)
+    # threshold the delta image, dilate the thresholded image to fill
+    # in holes, then find contours on thresholded image
+    thresh = cv2.threshold(frameDelta, conf["delta_thresh"], 255,
+                           cv2.THRESH_BINARY)[1]
+    thresh = cv2.dilate(thresh, None, iterations=2)
+    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+                            cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+
+    # loop over the contours
+    for c in cnts:
+        # if the contour is too small, ignore it
+        if cv2.contourArea(c) < conf["min_area"]:
             continue
 
-        # accumulate the weighted average between the current frame and
-        # previous frames, then compute the difference between the current
-        # frame and running average
-        cv2.accumulateWeighted(gray, avg, 0.5)
-        frame_delta = cv2.absdiff(gray, cv2.convertScaleAbs(avg))
+        # compute the bounding box for the contour, draw it on the frame,
+        # and update the text
+        (x, y, w, h) = cv2.boundingRect(c)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        text = "Occupied"
 
-        # threshold the delta image, dilate the thresholded image to fill
-        # in holes, then find contours on thresholded image
-        thresh = cv2.threshold(frame_delta, conf["delta_thresh"], 255,
-                               cv2.THRESH_BINARY)[1]
-        thresh = cv2.dilate(thresh, None, iterations=2)
-        cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-                                cv2.CHAIN_APPROX_SIMPLE)
-        cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+    # draw the text and timestamp on the frame
+    ts = timestamp.strftime("%A %d %B %Y %I:%M:%S%p")
+    cv2.putText(frame, "Room Status: {}".format(text), (10, 20),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+    cv2.putText(frame, ts, (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
+                0.35, (0, 0, 255), 1)
 
-        # loop over the contours
-        for c in cnts:
-            # if the contour is too small, ignore it
-            if cv2.contourArea(c) < conf["min_area"]:
-                continue
+    # check to see if the room is occupied
+    if text == "Occupied":
+        # check to see if enough time has passed between uploads
+        if (timestamp - lastUploaded).seconds >= conf["min_upload_seconds"]:
+            # increment the motion counter
+            motionCounter += 1
 
-            # compute the bounding box for the contour, draw it on the frame,
-            # and update the text
-            (x, y, w, h) = cv2.boundingRect(c)
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            text = "Occupied"
+            # check to see if the number of frames with consistent motion is
+            # high enough
+            if motionCounter >= conf["min_motion_frames"]:
+                # check to see if dropbox sohuld be used
+                if conf["use_dropbox"]:
+                    # write the image to temporary file
+                    t = TempImage()
+                    cv2.imwrite(t.path, frame)
 
-        # draw the text and timestamp on the frame
-        ts = timestamp.strftime("%A %d %B %Y %I:%M:%S%p")
-        cv2.putText(frame, "Room Status: {}".format(text), (10, 20),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-        cv2.putText(frame, ts, (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
-                    0.35, (0, 0, 255), 1)
-        # check to see if the room is occupied
-        if text == "Occupied":
-            # check to see if enough time has passed between uploads
-            if (timestamp - last_uploaded).seconds >= conf["min_upload_seconds"]:
-                # increment the motion counter
-                motion_counter += 1
+                    # upload the image to Dropbox and cleanup the tempory image
+                    print("[UPLOAD] {}".format(ts))
+                    path = "/{base_path}/{timestamp}.jpg".format(
+                        base_path=conf["dropbox_base_path"], timestamp=ts)
+                    client.files_upload(open(t.path, "rb").read(), path)
+                    t.cleanup()
 
-                # check to see if the number of frames with consistent motion is
-                # high enough
-                if motion_counter >= conf["min_motion_frames"]:
-                    # check to see if dropbox sohuld be used
-                    if conf["use_dropbox"]:
-                        # write the image to temporary file
-                        t = TempImage()
-                        cv2.imwrite(t.path, frame)
+                # update the last uploaded timestamp and reset the motion
+                # counter
+                lastUploaded = timestamp
+                motionCounter = 0
 
-                        # upload the image to Dropbox and cleanup the tempory image
-                        print("[UPLOAD] {}".format(ts))
-                        path = "/{base_path}/{timestamp}.jpg".format(
-                            base_path=conf["dropbox_base_path"], timestamp=ts)
-                        client.files_upload(open(t.path, "rb").read(), path)
-                        t.cleanup()
+    # otherwise, the room is not occupied
+    else:
+        motionCounter = 0
 
-                    # update the last uploaded timestamp and reset the motion
-                    # counter
-                    last_uploaded = timestamp
-                    motion_counter = 0
+    # check to see if the frames should be displayed to screen
+    if conf["show_video"]:
+        # display the security feed
+        cv2.imshow("Security Feed", frame)
+        key = cv2.waitKey(1) & 0xFF
 
-        # otherwise, the room is not occupied
-        else:
-            motion_counter = 0
-        # check to see if the frames should be displayed to screen
-        if conf["show_video"]:
-            # display the security feed
-            cv2.imshow("Security Feed", frame)
-            key = cv2.waitKey(1) & 0xFF
+        # if the `q` key is pressed, break from the lop
+        if key == ord("q"):
+            break
 
-            # if the `q` key is pressed, break from the lop
-            if key == ord("q"):
-                break
+    # clear the stream in preparation for the next frame
+    rawCapture.truncate(0)
 
-        # clear the stream in preparation for the next frame
-        raw_capture.truncate(0)
 '''
-
 import argparse
 
 ap = argparse.ArgumentParser()
@@ -156,5 +156,10 @@ ap.add_argument("-c", "--conf", required=True,
                 help="path to the JSON configuration file")
 args = vars(ap.parse_args())
 
+for x in range(2, 20000):
+    print(x)
+    print("-----")
+
 print("Ok I'm called by running a Docker command ..... voila!....")
 print(args)
+'''
