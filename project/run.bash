39686459bc7dc7429ca2aa27a12fbbcdf03b61eb
codeintheopen@gmail.com <codeintheopen@gmail.com>
2018-11-14 11:03:07 +0100

    added verbose information xauth

--- project/run.bash
+++ project/run.bash
@@ -9,8 +9,8 @@ sudo su root
 rm -Rf /root/.Xauthority
 touch /root/.Xauthority
 # Add the xauth cookie
-xauth -fv /root/.Xauthority add ${HOST}/unix:${DISPLAY_NUMBER} MIT-MAGIC-COOKIE-1 ${AUTH_COOKIE}
+xauth -f /root/.Xauthority add ${HOST}/unix:${DISPLAY_NUMBER} MIT-MAGIC-COOKIE-1 ${AUTH_COOKIE}
 # Copy the xauth file to the root
-cp /home/joesan/.Xauthority  /root/
-chmod 777 /root/
+#cp /home/joesan/.Xauthority  /root/
+#chmod 777 /root/
 python /var/raspi_motion_detection/project/core/motion_detector.py --conf conf/conf.json
