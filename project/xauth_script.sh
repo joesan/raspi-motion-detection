da66f1a33ad73e1661e0d3d316ac5627d9c7c466
codeintheopen@gmail.com <codeintheopen@gmail.com>
2018-11-14 06:12:26 +0100

    added xauth script

---
+++ project/xauth_script.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+CONTAINER_DISPLAY='0'
+CONTAINER_HOSTNAME='cctv'
+
+# Get the DISPLAY slot
+DISPLAY_NUMBER=$(echo $DISPLAY | cut -d. -f1 | cut -d: -f2)
+
+# Extract current authentication cookie
+AUTH_COOKIE=$(xauth list | grep "^$(hostname)/unix:${DISPLAY_NUMBER} " | awk '{print $3}')
+
+# Add the xauth cookie
+xauth add ${CONTAINER_HOSTNAME}/unix:${CONTAINER_DISPLAY} MIT-MAGIC-COOKIE-1 ${AUTH_COOKIE}
