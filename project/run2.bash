cd361791dbaa327383df3a393bfc1ffe68976ac5
codeintheopen@gmail.com <codeintheopen@gmail.com>
2018-11-14 10:41:35 +0100

    changes

--- project/run2.bash
+++ project/run2.bash
@@ -4,4 +4,4 @@ HOST=cctv
 DISPLAY_NUMBER=$(echo $DISPLAY | cut -d. -f1 | cut -d: -f2)
 AUTH_COOKIE=$(xauth list | grep "^$(hostname)/unix:${DISPLAY_NUMBER} " | awk '{print $3}')
 xauth add ${HOST}/unix:${DISPLAY_NUMBER} MIT-MAGIC-COOKIE-1 ${AUTH_COOKIE}
-python /opvart/raspi_motion_detection/project/core/motion_detector.py --conf conf/conf.json
+python /var/raspi_motion_detection/project/core/motion_detector.py --conf conf/conf.json
