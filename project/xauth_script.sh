1747fa4057e9d0e7b771b406b4726b1ef464fce3
codeintheopen@gmail.com <codeintheopen@gmail.com>
2018-11-14 10:15:29 +0100

    changes

--- project/xauth_script.sh
+++ project/xauth_script.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
-
 CONTAINER_DISPLAY='0'
 CONTAINER_HOSTNAME='cctv'
-
 echo $DISPLAY
 echo $(xauth list)
-
 # Get the DISPLAY slot
 DISPLAY_NUMBER=$(echo $DISPLAY | cut -d. -f1 | cut -d: -f2)
-
 # Extract current authentication cookie
 AUTH_COOKIE=$(xauth list | grep "^$(hostname)/unix:${DISPLAY_NUMBER} " | awk '{print $3}')
-
 sudo su root
 rm -Rf /root/.Xauthority
 touch /root/.Xauthority
 # Add the xauth cookie
 xauth -f /root/.Xauthority add ${CONTAINER_HOSTNAME}/unix:${CONTAINER_DISPLAY} MIT-MAGIC-COOKIE-1 ${AUTH_COOKIE}
-
 # Copy the xauth file to the root
 cp /home/joesan/.Xauthority  /root/
 chmod 777 /root/
